820fc20667a47be0b510f3fca84655496ba1dcac
Rodrigo55pereira <rodrigo55pereira@gmail.com>
2020-08-16 17:29:11 -0300

    atualização

---
+++ recebendo_dados_usuario.py
@@ -0,0 +1,17 @@
+"""
+Recebendo dados do usuário
+"""
+# Entrada de dados
+print("Qual seu nome? ")
+nome = input()  # input - Entrada
+
+print('Seja bem-vindo(a) %s' % nome)
+
+print("Qual sua idade? ")
+idade = input()
+
+# Processamento
+
+# Saída de dados
+print('A %s tem %s anos' % (nome, idade))
+
