4423a2ee85168bf0668741a0fd5277bcf2ba7987
Rodrigo55pereira <rodrigo55pereira@gmail.com>
2020-08-17 20:39:37 -0300

    finalizando sessão 3

--- recebendo_dados_usuario.py
+++ recebendo_dados_usuario.py
@@ -1,17 +1,49 @@
 """
 Recebendo dados do usuário
+input() -> Todo dado recebido via input é do tipo String
+
+Em Python, string é Tudo que estiver entre:
+- Aspas Simples;
+- Aspas duplas,
+- Aspas simples triplas;
+- Aspas duplas triplas;
+
 """
 # Entrada de dados
-print("Qual seu nome? ")
-nome = input()  # input - Entrada
+# print("Qual seu nome? ")
+# nome = input()  # input - Entrada
 
-print('Seja bem-vindo(a) %s' % nome)
+nome = input('Qual o seu nome? ')
 
-print("Qual sua idade? ")
-idade = input()
+# Exemplo de print 'antigo' 2.x
+# print('Seja bem-vindo(a) %s' % nome)
+
+# Exemplo de print 'moderno' 3.x
+# print('Seja bem-vindo(a) {0}'.format(nome))
+
+# Exemplo de print 'mais atual' 3.7
+print(f'Seja bem-vindo(a) {nome}')
+
+# print("Qual sua idade? ")
+# idade = input()
+
+idade = int(input('Qual sua idade? '))
 
 # Processamento
 
 # Saída de dados
-print('A %s tem %s anos' % (nome, idade))
+# Exemplo de print 'antigo' 2.x
+# print('A %s tem %s anos' % (nome, idade))
+# print('A {0} tem {1} anos'.format(nome, idade))
+print(f'A {nome} tem {idade} anos')
+
+"""
+# int(idade) => cast
+
+Cast é a 'conversão' de um tipo de dado para outro.
+"""
+
+print(f'A {nome} nasceu em {2020 - idade}')
+
+
 
